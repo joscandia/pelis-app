4e01559bc31495b7beba474e8b66966753071574
Jose Candia <joscandia98@gmail.com>
2024-02-10 11:15:41 -0300

    agg de tarjetasa y estilos css en la lista

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,37 +1,36 @@
 // components/MovieList.tsx
 import React from 'react';
-//import { Link } from 'react-router-dom';
 
 interface Movie {
-    Title: string;
-    Year: string;
-    imdbID: string; 
-    // Add more properties as needed
+  Title: string;
+  Year: string;
+  imdbID: string;
+}
+
+interface MovieListProps {
+  movies: Movie[] | undefined;
+  onMovieClick: (imdbID: string) => void; 
+}
+
+const MovieList: React.FC<MovieListProps> = ({ movies, onMovieClick }) => {
+  if (!movies) {
+    return <p>No movies available</p>;
   }
 
-  interface MovieListProps {
-    movies: Movie[] | undefined;
-  }
-  
-  const MovieList: React.FC<MovieListProps> = ({ movies }) => {
-    // Check if movies is defined before using map
-    if (!movies) {
-      return <p>No movies available</p>;
-    }
-  
-    return (
-      <div>
-        {movies.map((movie) => (
-          // Your mapping logic here
-          <div key={movie.imdbID}>
-            {/* Display movie details */}
-            <h3>{movie.Title}</h3>
-            <p>{movie.Year}</p>
-            {/* Add more details as needed */}
-          </div>
-        ))}
-      </div>
-    );
-  };
-  
-  export default MovieList;
+  return (
+    <div className="movie-list">
+      {movies.map((movie) => (
+        <button
+          key={movie.imdbID}
+          className="movie-card"
+          onClick={() => onMovieClick(movie.imdbID)}
+        >
+          <h3>{movie.Title}</h3>
+          <p>Año: {movie.Year}</p>
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
