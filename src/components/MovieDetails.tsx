4a92b56ae74caaf17b69db90f209a27b8981de9b
Jose Candia <joscandia98@gmail.com>
2024-02-10 10:40:11 -0300

    correccion de listas

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,11 +1,23 @@
-// components/MovieDetail.tsx
 import React from 'react';
 
+interface Movielist {
+  Title: string;
+  Rating: number;
+  Director: string;
+  Actors: string;
+  Plot: string; // Agrega Plot si también es una propiedad de las películas
+  Poster: string; // Agrega Poster si también es una propiedad de las películas
+}
+
 interface MovieDetailProps {
-  movie: any;
+  movie: Movielist | undefined; // Cambia a un solo objeto Movielist en lugar de un arreglo
 }
 
 const MovieDetail: React.FC<MovieDetailProps> = ({ movie }) => {
+  if (!movie) {
+    return <div>No se encontró la película.</div>;
+  }
+
   return (
     <div>
       <img src={movie.Poster} alt={movie.Title} />
