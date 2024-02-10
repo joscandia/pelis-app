63d4dae1355f5c18a2334fee5d4b2ab9690c3185
Jose Candia <joscandia98@gmail.com>
2024-02-10 12:28:42 -0300

    matrial ui implementation

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,6 +1,10 @@
 // components/MovieList.tsx
 import React from 'react';
-import '../css/movielist.css'
+//import '../css/movielist.css'
+import { Button, Card, CardContent, Typography } from '@mui/material';
+import { green } from '@mui/material/colors';
+
+const color = green [600];
 
 interface Movie {
   Title: string;
@@ -19,19 +23,23 @@ const MovieList: React.FC<MovieListProps> = ({ movies, onMovieClick }) => {
   }
 
   return (
-    <div className="movie-list">
-      {movies.map((movie) => (
-        <button
-          key={movie.imdbID}
-          className="movie-card"
-          onClick={() => onMovieClick(movie.imdbID)}
-        >
-          <h3>{movie.Title}</h3>
-          <p>Año: {movie.Year}</p>
-        </button>
-      ))}
-    </div>
-  );
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '140vh', flexDirection: 'column' }}>
+    {movies.map((movie) => (
+      <Card key={movie.imdbID} style={{ margin: '10px', width: '200px' }}>
+        <CardContent>
+          <Typography variant="h6" component="div">
+            {movie.Title}
+          </Typography>
+          <Typography color="textSecondary">
+            Año: {movie.Year}
+          </Typography>
+          <Button  onClick={() => onMovieClick(movie.imdbID)} variant="contained" color="success">
+            Detalles
+          </Button>
+        </CardContent>
+      </Card>
+    ))}
+  </div>
+);
 };
-
 export default MovieList;
